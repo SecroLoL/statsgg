76b665cce155681324b94a6fbd58f9c69476c75e
JCamyre <JWcamry03@gmail.com>
2021-08-24 22:31:01 -0700

    Add views logic and url paths

--- reactsite/reactsite/settings.py
+++ reactsite/reactsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from secret_keys import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#2s5mq++nl&puh$45lji!9$$zgnaj^8ufp2uew#hoxzr&82*)q'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- reactsite/stats/views.py
+++ reactsite/stats/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from .models import Match
+from .serializers import MatchSerializer
+from rest_framework import generics, status
 # Create your views here.
+
+class MatchView(generics.ListAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+    
+    
