ba6622f7fa8d95d1b6027f1485a0a18315616350
JCamyre <JWcamry03@gmail.com>
2021-08-25 18:38:04 -0700

    Add react home and about page

--- reactsite/frontend/src/App.js
+++ reactsite/frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
+// Have React handle all url patterns for frontend, don't need urls.py
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </Router>
+ );
 }
 
 export default App;
---
+++ reactsite/frontend/src/components/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Home(){
+
+    return (
+        <div>
+            <h1>Home Page!</h1>
+        </div>
+    )
+}
