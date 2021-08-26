6f2d6eab441fb8ee736611a9ac421c2b32bc3b48
JCamyre <JWcamry03@gmail.com>
2021-08-26 13:20:25 -0700

    Update admin.py to show models and tweak models.py

--- reactsite/stats/admin.py
+++ reactsite/stats/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Champion, Player
 
 # Register your models here.
+admin.site.register(Champion)
+admin.site.register(Player)
--- reactsite/stats/models.py
+++ reactsite/stats/models.py
@@ -1,14 +1,18 @@
-from djongo import models
+from django.db import models
 from django import forms
 
 # To update these models to the database, py manage.py makemigrations/migrate
 # The format for PostgreSQL "relations"/'models'/collections/columns is ex: stats_player, stats_champion. 
-# So it takes react app name + _ + lower case name of class
+# So it takes react app name + _ + lower case name of class (case ignored). Spaces are also ignored
 
 # SQL syntax
 # \d; to get all info of database
 # SELECT * FROM <model_name>; get all from a specific model
+# INSERT INTO <model_name>(<fields>) VALUES <values>. THE REASON WHY IT WILL SAY column "EBOMBERR" does not exist is because
+# You have to use '' instead of ""
+# 
 
+# So what if mongoDB did work, just didn't show cause of admin.py
 
 # Create your models here.
 class Player(models.Model):
@@ -17,9 +21,15 @@ class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.username
+    
 class Champion(models.Model):
     
     champion_id = models.AutoField(primary_key=True)
+    
+    def __str__(self):
+        return f'Champion {str(self.champion_id)}'
     
 class Yo(models.Model):
     
