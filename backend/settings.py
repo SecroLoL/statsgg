ef0886c9871f025ace8959d85646ad8cdc2674cf
JCamyre <JWcamry03@gmail.com>
2021-09-06 10:16:31 -0700

    Initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -92,12 +92,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
