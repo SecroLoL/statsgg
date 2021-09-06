86de6cbaa0741962f3b9d76e33acd1f18587266e
JCamyre <JWcamry03@gmail.com>
2021-09-06 11:46:43 -0700

    Add basic page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from "./logo.svg";
+import React from "react";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
 
@@ -7,22 +9,11 @@ axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
