b4a59d4dce0aaafb6a76777d9728a27aadc24c1c
JCamyre <JWcamry03@gmail.com>
2021-09-06 12:19:43 -0700

    Add basic page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./components/pages/Home";
+import Home from "./components/pages/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
