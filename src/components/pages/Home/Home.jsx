8898e863c16f777564a1f234909afa40cfd9fbe6
Joseph Camyre <jwcamry03@gmail.com>
2021-09-07 19:31:23 -0700

    Add code for displaying database models

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,19 +6,25 @@ function Home() {
 
     useEffect(() => {
         axios.get('http://localhost:8000/stats/matches').then((matches) => {
-            console.log(matches);
-            if (matches) {
-                setMatches(matches)
+            if (matches.data) {
+                console.log(matches.data);
+                setMatches(matches.data);
             }
         }
         )
-    });
+    }, []); // I'm assuming the , [] is alternate to when page is loaded.
 
     return (
         <div>
             <h1>Stats.gg</h1>
-            {matches.data.map((match) => (
-                <h1>match.championName</h1>
+            {matches.map((match) => (
+                <div key={match.totalDamageDealt}>
+                    <h1>{match.championName}</h1>
+                    <h2>Kda: <h3><span style={{color: 'green'}}>{match.kills}</span> / <span>{match.deaths}</span> / <span>{match.assists}</span> <span style={{color: {}}}>({match.deaths > 0 ? (match.kills + match.assists) / match.deaths : (match.kills + match.assists).toFixed(1)})</span></h3></h2>
+                    <h2>Damage: <h3>{match.totalDamageDealt}</h3></h2>
+
+                </div>
+
             ))}
         </div>
     )
